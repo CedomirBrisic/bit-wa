f6b54c4b020e9f658b62183b92dbb1364ed56f3f
CedomirBrisic <cedabrisic@gmail.com>
2018-03-18 19:58:17 +0100

    ...

--- excercises/ReactUsersExtra/app.js
+++ excercises/ReactUsersExtra/app.js
@@ -13,33 +13,66 @@ const ListOfUsers = (props) => {
 
 
     return (
-        <ul class="collection">
+        <div className="row">
             {
                 props.data.map(element => {
                     return <User data={element} />
                 })
             }
 
-        </ul>
+        </div>
 
 
     )
 }
 
+const dateOfBirth = (dateInput) => {
+    const date = new Date(dateInput);
+    const fullMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let dd = date.getDate();
+    let mm = date.getMonth();
+    let yy = date.getFullYear();
+    return `${fullMonth[mm]} - ${dd} - ${yy}`
+}
 
+
+const Dob = (props) => {
+
+
+    return (
+        dateOfBirth(props.data)
+    )
+}
+
+const asterixMail = (inputMail) => {
+    let output = inputMail.split("");
+    output.splice(3, 5, "*", "*", "*", "*", "*");
+    return output;
+}
+
+const MailHidden = (props) => {
+    return (
+        asterixMail(props.data)
+    )
+}
 
 const User = (props) => {
     const user = props.data;
     return (
-        <li class="collection-item avatar">
-            <img src={user.picture.thumbnail} alt="" class="circle" />
-            <span class="title">{user.name.first}</span>
-            <p> {user.email}<br />
-                {user.dob}
 
-            </p>
 
-        </li>
+        <div className="col s12 m4">
+            <div className="card">
+                <div className="card-image">
+                    <img src={user.picture.large} />
+                    <span className="card-title">{user.name.first}</span>
+                </div>
+                <div className="card-content">
+                    <p>Email: <MailHidden data={user.email} /> <br />
+                        Date of birth: <Dob data={user.dob} /> </p>
+                </div>
+            </div>
+        </div>
 
     )
 
@@ -48,19 +81,13 @@ const User = (props) => {
 const Footer = () => {
     return (
 
-        <footer class="page-footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col l6 s12">
-                        <h5 class="white-text">Footer Content</h5>
-                        <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-                    </div>
-
+        <footer className="page-footer">
+            <div className="row">
+                <div className="col l6 s12">
+                    <p className="grey-text text-lighten-4">© 2018 Copyright 2018</p>
                 </div>
             </div>
-            <div class="footer-copyright">
 
-            </div>
         </footer>)
 }
 
