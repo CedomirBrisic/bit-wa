81f068f32e3b872ca6d9e6087fb31ba1e777a76b
CedomirBrisic <cedabrisic@gmail.com>
2018-03-22 21:39:03 +0100

    info page all date

--- BITshow/bitShow React/bitshow/src/app/partials/Info.js
+++ BITshow/bitShow React/bitshow/src/app/partials/Info.js
@@ -1,48 +1,79 @@
 import React from "react"
-import fragment from "react-router-dom";
+import fragment from "react-router-dom"
+import Header from "./Header"
+import Footer from "./Footer"
 
 class Info extends React.Component {
-    constructor (props){
-        super (props)
+    constructor(props) {
+        super(props)
 
         this.state = {
-            showImage : "",
+            showImage: "",
             showTitle: "",
             showSummary: "",
-            showSeason : "",
+            showSeasonCount: 0,
+            startEnd: "",
+            cast: ""
 
         }
     }
 
-        componentDidMount(){
-            fetch(`http://api.tvmaze.com/shows/${this.props.match.params.showIdForFetch}`)
+    componentDidMount() {
+        fetch(`http://api.tvmaze.com/shows/${this.props.match.params.showIdForFetch}`)
             .then((response) => {
                 return response.json()
-            }).then ((response) => {
+            }).then((response) => {
                 this.setState({
                     showImage: response.image.original,
                     showTitle: response.name,
-                    showSummary :response.summary.replace(/<\/?[^>]+(>|$)/g, "")
+                    showSummary: response.summary.replace(/<\/?[^>]+(>|$)/g, "")
                 })
             })
-            
-            fetch(`http://api.tvmaze.com/shows/${}/seasons`)
-        }
 
-        render (){
-            // console.log()
-            return (
-                <React.Fragment>
+        fetch(`http://api.tvmaze.com/shows/${this.props.match.params.showIdForFetch}/seasons`)
+            .then((response) => {
+                return response.json()
+            }).then((response) => {
+                this.setState({
+                    showSeasonCount: (response.length),
+                    startEnd: response.map((element) => {
+                        return <li> {element.premiereDate} - {element.endDate} </li>
+                    })
+                })
+            })
+
+        fetch(`http://api.tvmaze.com/shows/${this.props.match.params.showIdForFetch}/cast`)
+            .then((response) => {
+                return response.json()
+            }).then((response) => {
+                this.setState({
+                    cast: response.map((element) => {
+                        return <li> {element.person.name} </li>
+                    })
+                })
+            })
+    }
+
+    render() {
+        // console.log()
+        return (
+            <React.Fragment>
+            <Header />
                 <div className="container">
-                <h1>{this.state.showTitle}</h1>
-            
-                <img src={this.state.showImage} alt="" />
-                <h3> </h3>
+                    <h1>{this.state.showTitle}</h1>
+
+                    <img src={this.state.showImage} alt="" />
+                    <h3> Number Of Seasons: {this.state.showSeasonCount}</h3>
+                    <ul>{this.state.startEnd}</ul>
+                    <h3>Cast: </h3>
+                    <ul>{this.state.cast}</ul>
+                    <h3>Sumarry:</h3>
+                    <p> {this.state.showSummary}</p>
                 </div>
-                <p> {this.state.showSummary}</p>
+                <Footer />
             </React.Fragment>
-            )
-        }
+        )
+    }
 
 }
 
